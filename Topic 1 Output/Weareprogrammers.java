5d51e1f2049624e92efb52674124134071819bd2
KanapongAiamtip <kannaphongi62@nu.ac.th>
2021-07-04 23:50:13 +0700

    Topic 1: Output

---
+++ Topic 1 Output/Weareprogrammers.java
@@ -0,0 +1,7 @@
+public class Weareprogrammers {
+    public static void main(String[] args) {
+        System.out.println("We");
+        System.out.println("are");
+        System.out.println("programmers");
+    }
+}
