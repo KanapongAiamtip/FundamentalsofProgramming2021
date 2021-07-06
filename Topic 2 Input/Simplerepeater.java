0b17d7c81358757ffb7386c24bd0ee7d3b723843
KanapongAiamtip <kannaphongi62@nu.ac.th>
2021-07-07 03:07:58 +0700

    Topic 2: Input

---
+++ Topic 2 Input/Simplerepeater.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Simplerepeater {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String x = in.nextLine();
+        System.out.println(x);
+        System.out.println(x);
+    }
+}
