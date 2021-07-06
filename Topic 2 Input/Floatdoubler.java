a158c3ab6e9cae548e04f3c204bc7fa7c2c5762b
KanapongAiamtip <kannaphongi62@nu.ac.th>
2021-07-07 03:09:38 +0700

    Topic 2: Input

---
+++ Topic 2 Input/Floatdoubler.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Floatdoubler {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        float x = in.nextFloat();
+        System.out.println(x * 2);
+    }
+
+}
