190950c23a9d3e092f706d159539fe65de243096
KanapongAiamtip <kannaphongi62@nu.ac.th>
2021-07-07 03:08:17 +0700

    Topic 2: Input

---
+++ Topic 2 Input/Inlinerepeater.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Inlinerepeater {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String x = in.nextLine();
+        System.out.println(x + " " + x);
+    }
+}
