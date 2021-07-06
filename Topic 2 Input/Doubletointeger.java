b392bd0fb37a25c83571cc4960e0819fff75d380
KanapongAiamtip <kannaphongi62@nu.ac.th>
2021-07-07 03:09:43 +0700

    Topic 2: Input

---
+++ Topic 2 Input/Doubletointeger.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Doubletointeger {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double x = in.nextDouble();
+        System.out.println((int) x);
+    }
+}
