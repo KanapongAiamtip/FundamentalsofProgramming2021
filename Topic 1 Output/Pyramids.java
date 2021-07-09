73205326262a2d9d48a587b868885f68a0eb1170
KanapongAiamtip <kannaphongi62@nu.ac.th>
2021-07-10 02:25:25 +0700

    Topic 1: Output

--- Topic 1 Output/Pyramids.java
+++ Topic 1 Output/Pyramids.java
@@ -1,7 +1,8 @@
 public class Pyramids {
     public static void main(String[] args) {
-        System.out.println("o o");
-        System.out.println(" ^");
-        System.out.println("\\_/");
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("*******");
     }
 }
