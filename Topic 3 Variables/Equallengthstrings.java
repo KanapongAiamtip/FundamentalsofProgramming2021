d9535eaae2782eaec59112ba0aa9f69b3c30aece
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-07-23 03:31:52 +0700

    Topic 3: Variables

---
+++ Topic 3 Variables/Equallengthstrings.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Equallengthstrings {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String x = in.nextLine();
+        String y = in.nextLine();
+        System.out.println(x.length()==y.length());
+    }
+}
