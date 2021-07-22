9f1e881bc8790da80e9a53b9643f822f3e558061
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-07-23 03:29:54 +0700

    Topic 3: Variables

---
+++ Topic 3 Variables/Averagelengthofstrings.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Averagelengthofstrings {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    String a = in.nextLine();
+    String b = in.nextLine();
+    String c = in.nextLine();
+    double sum = a.length() + b.length() + c.length();
+    double arr = sum / 3;
+    System.out.println(String.format("%.2f", arr));
+  }
+}
