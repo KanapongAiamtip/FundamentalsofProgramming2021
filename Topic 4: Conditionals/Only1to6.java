7c785dcd02e1f2595ca4da3023800d7819652925
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-07-29 23:54:31 +0700

    Topic 4 Conditionals

---
+++ Topic 4: Conditionals/Only1to6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ public class Only1to6 {
+     public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+         int x = in.nextInt();
+         if (x<=6) {
+            System.out.println("ok");
+         }
+         else{
+            System.out.println("");
+         }
+     }
+ }
