83ecb6071ea5d75e89800f8422c8020e12c7400f
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-08-21 21:58:46 +0700

    Fix Loop

--- Topic 6 Loops 2/Doubleuntilx.java
+++ Topic 6 Loops 2/Doubleuntilx.java
@@ -3,13 +3,8 @@ public class Doubleuntilx {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int Leon = sc.nextInt();
-        for (int i = 1; i <=Leon; i++) {
-            if(i==1)
-            {
-                System.out.println(1);
-            }
-            else if(Math.pow(2,i)<=Leon)
-            {
+        for (int i = 0; i <=Leon; i++) {
+            if(Math.pow(2,i)<=Leon){
                 System.out.println((int)(Math.pow(2,i)));
             } 
         }
