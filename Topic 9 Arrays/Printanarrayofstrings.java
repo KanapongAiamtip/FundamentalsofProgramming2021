87f8668341550323190a30a537b32b2ee38e1cb9
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-09-09 02:45:27 +0700

    Topic 9: Arrays

---
+++ Topic 9 Arrays/Printanarrayofstrings.java
@@ -0,0 +1,11 @@
+public class Printanarrayofstrings
+{
+    public static void main(String[] args) 
+    {
+        String[] names = {"Anna", "Beryl", "Clara", "Desmond", "Eleanor", "Fiona", "George", "Henry"};
+        for (int i = 0; i < names.length; i++) 
+        {
+            System.out.println(names[i]);    
+        }
+    }
+}
