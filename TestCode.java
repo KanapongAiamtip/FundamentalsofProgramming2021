b433d714896787aa46e6b1f705ae6298227574e8
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-10-23 03:28:56 +0700

    TestCode

--- TestCode.java
+++ TestCode.java
@@ -1,9 +1,5 @@
-import java.util.Scanner;
-public class TestCode{
-    public static void main(String[] args) 
-    {
-        Scanner in = new Scanner(System.in);
-        int x = in.nextInt();
-        System.out.println();
+public class TestCode {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
     }
 }
