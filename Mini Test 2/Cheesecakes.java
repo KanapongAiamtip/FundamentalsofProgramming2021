d8839a0e565af92869ae66e82343d4dd5c729211
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-10-23 03:25:12 +0700

    Mini Test 2

---
+++ Mini Test 2/Cheesecakes.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Cheesecakes {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int Leon = in.nextInt();
+        System.out.println(Leon*50);
+    }
+}
